85a2e28b0d0e8cac65f35559c9b1bb4ed64b2bce
Andrey <geass1000@gmail.com>
2020-02-15 18:41:22 +0300

    feat(app): use `Core` module, `Keyboard` component and `KeyEvent` directive in `App` module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Feature Modules
+import { CoreModule } from './core/core.module';
+
+const modules = [
+  CoreModule,
+];
+
+// Components
 import { AppComponent } from './app.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
+
+const components = [
+  AppComponent,
+  KeyboardComponent,
+];
+
+// Directives
+import { KeyEventDirective } from './shared/key-event-directive';
+
+const directives = [
+  KeyEventDirective,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ...modules,
+  ],
+  declarations: [
+    ...components,
+    ...directives
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ]
 })
 export class AppModule { }
