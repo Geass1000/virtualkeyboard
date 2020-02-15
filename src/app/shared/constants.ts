bf5f7a406a9190769551688516835dad174d36a3
Andrey <geass1000@gmail.com>
2020-02-15 18:32:04 +0300

    feat(shared): add `Keyboard Layout` constant

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,1083 @@
+import * as Enums from './enums';
+import * as Interfaces from './interfaces';
+
+export const KeyboardLayout: Interfaces.KeyboardLayoutElement[] = [
+  {
+    key: Enums.KeyCode.Escape,
+    position: {
+      x: 0,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F1,
+    position: {
+      x: 2,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F2,
+    position: {
+      x: 4,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F3,
+    position: {
+      x: 6,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F4,
+    position: {
+      x: 8,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F5,
+    position: {
+      x: 10,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F6,
+    position: {
+      x: 12,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F7,
+    position: {
+      x: 14,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F8,
+    position: {
+      x: 16,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F9,
+    position: {
+      x: 18,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F10,
+    position: {
+      x: 20,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F11,
+    position: {
+      x: 22,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.F12,
+    position: {
+      x: 24,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.Insert,
+    position: {
+      x: 26,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.Delete,
+    position: {
+      x: 28,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.PageUp,
+    position: {
+      x: 30,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.PageDown,
+    position: {
+      x: 32,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.Home,
+    position: {
+      x: 34,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.End,
+    position: {
+      x: 36,
+      y: 0,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.Backquote,
+    position: {
+      x: 0,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit1,
+    position: {
+      x: 2,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit2,
+    position: {
+      x: 4,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit3,
+    position: {
+      x: 6,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit4,
+    position: {
+      x: 8,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit5,
+    position: {
+      x: 10,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit6,
+    position: {
+      x: 12,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit7,
+    position: {
+      x: 14,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit8,
+    position: {
+      x: 16,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit9,
+    position: {
+      x: 18,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Digit0,
+    position: {
+      x: 20,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Minus,
+    position: {
+      x: 22,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Equal,
+    position: {
+      x: 24,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Backspace,
+    position: {
+      x: 26,
+      y: 1,
+    },
+    size: {
+      width: 3,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.NumLock,
+    position: {
+      x: 30,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.NumpadDivide,
+    position: {
+      x: 32,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.NumpadMultiply,
+    position: {
+      x: 34,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.NumpadSubtract,
+    position: {
+      x: 36,
+      y: 1,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Tab,
+    position: {
+      x: 0,
+      y: 3,
+    },
+    size: {
+      width: 3,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyQ,
+    position: {
+      x: 3,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyW,
+    position: {
+      x: 5,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyE,
+    position: {
+      x: 7,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyR,
+    position: {
+      x: 9,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyT,
+    position: {
+      x: 11,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyY,
+    position: {
+      x: 13,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyU,
+    position: {
+      x: 15,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyI,
+    position: {
+      x: 17,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyO,
+    position: {
+      x: 19,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyP,
+    position: {
+      x: 21,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.BracketLeft,
+    position: {
+      x: 23,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.BracketRight,
+    position: {
+      x: 25,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Backslash,
+    position: {
+      x: 27,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad7,
+    position: {
+      x: 30,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad8,
+    position: {
+      x: 32,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad9,
+    position: {
+      x: 34,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.NumpadAdd,
+    position: {
+      x: 36,
+      y: 3,
+    },
+    size: {
+      width: 2,
+      height: 4,
+    },
+  },
+  {
+    key: Enums.KeyCode.CapsLock,
+    position: {
+      x: 0,
+      y: 5,
+    },
+    size: {
+      width: 3,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyA,
+    position: {
+      x: 3,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyS,
+    position: {
+      x: 5,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyD,
+    position: {
+      x: 7,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyF,
+    position: {
+      x: 9,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyG,
+    position: {
+      x: 11,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyH,
+    position: {
+      x: 13,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyJ,
+    position: {
+      x: 15,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyK,
+    position: {
+      x: 17,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyL,
+    position: {
+      x: 19,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Semicolon,
+    position: {
+      x: 21,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Quote,
+    position: {
+      x: 23,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Enter,
+    position: {
+      x: 25,
+      y: 5,
+    },
+    size: {
+      width: 4,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad4,
+    position: {
+      x: 30,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad5,
+    position: {
+      x: 32,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad6,
+    position: {
+      x: 34,
+      y: 5,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.ShiftLeft,
+    position: {
+      x: 0,
+      y: 7,
+    },
+    size: {
+      width: 4,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyZ,
+    position: {
+      x: 4,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyX,
+    position: {
+      x: 6,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyC,
+    position: {
+      x: 8,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyV,
+    position: {
+      x: 10,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyB,
+    position: {
+      x: 12,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyN,
+    position: {
+      x: 14,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.KeyM,
+    position: {
+      x: 16,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Comma,
+    position: {
+      x: 18,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Period,
+    position: {
+      x: 20,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Slash,
+    position: {
+      x: 22,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.ShiftRight,
+    position: {
+      x: 24,
+      y: 7,
+    },
+    size: {
+      width: 5,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad1,
+    position: {
+      x: 30,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad2,
+    position: {
+      x: 32,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad3,
+    position: {
+      x: 34,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.NumpadEnter,
+    position: {
+      x: 36,
+      y: 7,
+    },
+    size: {
+      width: 2,
+      height: 4,
+    },
+  },
+  {
+    key: Enums.KeyCode.ControlLeft,
+    position: {
+      x: 0,
+      y: 9,
+    },
+    size: {
+      width: 4,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.AltLeft,
+    position: {
+      x: 4,
+      y: 9,
+    },
+    size: {
+      width: 3,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.Space,
+    position: {
+      x: 7,
+      y: 9,
+    },
+    size: {
+      width: 9,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.AltRight,
+    position: {
+      x: 16,
+      y: 9,
+    },
+    size: {
+      width: 3,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.ControlRight,
+    position: {
+      x: 19,
+      y: 9,
+    },
+    size: {
+      width: 4,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.ArrowUp,
+    position: {
+      x: 25,
+      y: 9,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.ArrowDown,
+    position: {
+      x: 25,
+      y: 10,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.ArrowLeft,
+    position: {
+      x: 23,
+      y: 10,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.ArrowRight,
+    position: {
+      x: 27,
+      y: 10,
+    },
+    size: {
+      width: 2,
+      height: 1,
+    },
+  },
+  {
+    key: Enums.KeyCode.Numpad0,
+    position: {
+      x: 30,
+      y: 9,
+    },
+    size: {
+      width: 4,
+      height: 2,
+    },
+  },
+  {
+    key: Enums.KeyCode.NumpadDecimal,
+    position: {
+      x: 34,
+      y: 9,
+    },
+    size: {
+      width: 2,
+      height: 2,
+    },
+  },
+];
