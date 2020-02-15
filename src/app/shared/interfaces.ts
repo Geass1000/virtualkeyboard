bfc777989b78e09e985cb80344ba0fec3fd5514e
Andrey <geass1000@gmail.com>
2020-02-15 18:31:38 +0300

    feat(shared): add `Keyboard Layout Element` interface

---
+++ src/app/shared/interfaces.ts
@@ -0,0 +1,17 @@
+import * as Enums from './enums';
+
+export interface KeyboardLayoutElementPosition {
+  x: number;
+  y: number;
+}
+
+export interface KeyboardLayoutElementSize {
+  width: number;
+  height: number;
+}
+
+export interface KeyboardLayoutElement {
+  key: Enums.KeyCode;
+  position: KeyboardLayoutElementPosition;
+  size: KeyboardLayoutElementSize;
+}
