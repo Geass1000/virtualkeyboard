1d8f42e1bb4db61b85d98291bbadecc1953e3d37
Andrey <geass1000@gmail.com>
2020-02-15 18:42:01 +0300

    feat(app): add `Key Event Handler` logic and show `Keyboard` component`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'vkb';
+export class AppComponent implements OnInit {
+  private pressedKeysMap: Map<string, boolean>;
+  public pressedKeysList: string[];
+
+  ngOnInit () {
+    this.pressedKeysMap = new Map();
+    this.updateListOfPressedKeys();
+  }
+
+  /**
+   * Handles window Key Up event.
+   *
+   * @param   {KeyboardEvent} event
+   * @returns {void}
+   */
+  onKeyUp (
+    event: KeyboardEvent,
+  ): void {
+    console.log(`KeyUP`, event);
+    this.pressedKeysMap.set(event.code, false);
+    event.preventDefault();
+    this.updateListOfPressedKeys();
+  }
+
+  /**
+   * Handles window Key Down event.
+   *
+   * @param   {KeyboardEvent} event
+   * @returns {void}
+   */
+  onKeyDown (
+    event: KeyboardEvent,
+  ): void {
+    console.log(`KeyDOWN`, event);
+    this.pressedKeysMap.set(event.code, true);
+    event.preventDefault();
+    this.updateListOfPressedKeys();
+  }
+
+  /**
+   * Updates list of pressed keys.
+   *
+   * @returns {void}
+   */
+  updateListOfPressedKeys (
+  ): void {
+    const pressedKeys = [];
+
+    this.pressedKeysMap.forEach((value, key) => {
+      if (value === true) {
+        pressedKeys.push(key);
+      }
+    });
+
+    this.pressedKeysList = pressedKeys;
+  }
 }
